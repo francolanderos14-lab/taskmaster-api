af9bb7ffef475e9ef5de6bc39471ddad243f03ec
francolanderos14-lab <Francolanderos14@gmail.com>
2026-07-01 19:46:28 -0400

    feat: migrar el estado de Terraform a backend remoto en S3

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -7,6 +7,14 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket       = "taskmaster-terraform-state-345843270501"
+    key          = "taskmaster-api/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+    encrypt      = true
+  }
 }
 
 provider "aws" {
